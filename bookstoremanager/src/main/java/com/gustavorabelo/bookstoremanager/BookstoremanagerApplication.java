f9bd9df4d8c4424539e13d97fded75c8acfcb92d
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-02-07 07:08:43 -0300

    Primeiro commit

---
+++ bookstoremanager/src/main/java/com/gustavorabelo/bookstoremanager/BookstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.gustavorabelo.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanagerApplication.class, args);
+	}
+
+}
